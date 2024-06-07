8f104f207213f25e309d915b0be323e69d5d7878
kareemalmosafi <THEKAREEMBO@gmail.com>
2024-06-07 21:30:21 +0300

    Acitvate Redis in Deployment issue 5.0

--- src/main/java/LibraryManagementSystem/ManagingPackage/Caching/RedisConfig.java
+++ src/main/java/LibraryManagementSystem/ManagingPackage/Caching/RedisConfig.java
@@ -1,29 +1,38 @@
 package LibraryManagementSystem.ManagingPackage.Caching;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SslOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.io.IOException;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableRedisRepositories
 public class RedisConfig
 {
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory)
-    {
-        RedisCacheConfiguration redisCacheConfiguration= RedisCacheConfiguration.defaultCacheConfig()
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(60))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
@@ -34,25 +43,48 @@ public class RedisConfig
     }
 
 
+    @org.springframework.beans.factory.annotation.Value("${spring.data.redis.host}")
+    private String host;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.data.redis.port}")
+    private int port;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.data.redis.password}")
+    private String password;
+
+    @org.springframework.beans.factory.annotation.Value("${spring.data.redis.ssl.enabled}")
+    private boolean sslEnabled;
+
+    private final ResourceLoader resourceLoader;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
+    RedisConnectionFactory redisConnectionFactory() throws IOException {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName("oregon-redis.render.com");
-        redisStandaloneConfiguration.setPort(6379);
-        redisStandaloneConfiguration.setPassword("j651XdbevVM1sDv00P1al30oKdUHl7by");
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
 
-        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
-        jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60)); // connection timeout
-        jedisClientConfiguration.useSsl();
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder lettuceClientConfigurationBuilder =
+                LettuceClientConfiguration.builder();
 
-        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration.build());
-    }
+        if (sslEnabled){
+            SslOptions sslOptions = SslOptions.builder()
+                    .trustManager(resourceLoader.getResource("classpath:redis.pem").getFile())
+                    .build();
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
+            ClientOptions clientOptions = ClientOptions
+                    .builder()
+                    .sslOptions(sslOptions)
+                    .protocolVersion(ProtocolVersion.RESP3)
+                    .build();
+
+            lettuceClientConfigurationBuilder
+                    .clientOptions(clientOptions)
+                    .useSsl();
+        }
+
+        LettuceClientConfiguration lettuceClientConfiguration = lettuceClientConfigurationBuilder.build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
     }
 }
